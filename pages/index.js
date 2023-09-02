896bb68b509129ffef2cf3062b0c7b0ea2d94540
DengelLionel <dengelriveracajincho@gmail.com>
2023-09-02 17:03:01 -0500

    crea

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Crea Tienda Online | Expertos en eCommerce | Crea Tienda Ya</title>
+				<title>Creamos tu tienda Online en Crea Tienda Ya</title>
 				<meta name="description" content="En Crea Tienda Ya, convertimos tus sueños en realidad digital. Como expertos en la creación y crecimiento de tiendas en línea, estamos comprometidos en ayudarte a lanzar y hacer prosperar tu negocio en línea de manera efectiva y exitosa. ¡Descubre cómo podemos hacer que tu visión cobre vida en el mundo digital hoy mismo!"/>
-				<meta name="keywords" content="crea tienda online, expertos eCommerce, eCommerce, tienda en línea, creación de tiendas, negocio en línea,dropshipping"/>
+				<meta name="keywords" content="crear tienda online,tienda,tienda virtual, tiendas virtuales peru,tiendas virtuales gratis, tienda online, eCommerce, tienda en línea, creación de tiendas, negocio en línea,dropshipping"/>
 				<meta name="robots" content="index, follow"/>
 			</Head>
 			<Hero/>
