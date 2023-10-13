82c0b11cba5c89acf97b7ec4a2a15d5edf9b3f28
DengelLionel <dengelriveracajincho@gmail.com>
2023-10-13 01:01:02 -0500

    a

--- components/Hero/SliderService.js
+++ components/Hero/SliderService.js
@@ -4,21 +4,21 @@ import 'react-slideshow-image/dist/styles.css';
 
 const SliderService = () => {
     const images = [
-        "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-        "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-        "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+        "https://res.cloudinary.com/dh9etf988/image/upload/v1697176533/creatiendaya/tu-tienda-online-shopify-diseno-personalizado-diferente-autentico_zknhwc.png",
+        "https://res.cloudinary.com/dh9etf988/image/upload/v1697176546/creatiendaya/tienda_ii_bpqzom.png",
+      
     ];
   return (
-    <Zoom  autoplay={true} scale={1.4} arrows={false} infinite={true} indicators={true}>
+    <Zoom  autoplay={true} scale={1.4} arrows={true} infinite={true} indicators={true}>
     {images.map((each, index) => (
         <div>
             {/* Mobile */}
         <div className='d-block d-lg-none' key={index} style={{ width: "100%" }}>
-            <img style={{ objectFit: "cover", width: "100%",height:"550px" }} alt="Slide Image" src={each} />
+            <img style={{ objectFit: "cover", width: "100%",height:"550px" }} alt="Creamos y mejoramos tienda online shopify" src={each} />
         </div>
         {/* Desktop */}
          <div className='d-none d-lg-block' key={index} style={{ width: "100%" }}>
-         <img style={{ objectFit: "cover", width: "100%",height:"650px" }} alt="Slide Image" src={each} />
+         <img style={{ objectFit: "cover", width: "100%",height:"650px" }} alt="Creamos y mejoramos tienda online shopify" src={each} />
      </div>
      </div>
     ))}
